169d4e353efc5b3d711a3983209bc616e2089c3e
Lorraine <lorrainejanecostelo@gmail.com>
2021-04-01 15:58:37 +0800

    commit

--- vue.js
+++ vue.js
@@ -74,3 +74,24 @@ const Counter = {
   }
   
   Vue.createApp(ListRendering).mount('#list-rendering')
+
+  const TodoList = {
+    data() {
+      return {
+        groceryList: [
+          { id: 0, text: 'Vegetables' },
+          { id: 1, text: 'Cheese' },
+          { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+      }
+    }
+  }
+  
+  const app = Vue.createApp(TodoList)
+  
+  app.component('todo-item', {
+    props: ['todo'],
+    template: `<li>{{ todo.text }}</li>`
+  })
+  
+  app.mount('#todo-list-app')
