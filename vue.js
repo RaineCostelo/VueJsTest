3b9632f01f8a6ec7b09993e18dd3f3c71d88566f
Lorraine <lorrainejanecostelo@gmail.com>
2021-04-01 13:13:31 +0800

    commit changes

--- vue.js
+++ vue.js
@@ -40,3 +40,13 @@ const Counter = {
   }
   
   Vue.createApp(EventHandling).mount('#event-handling')
+
+  const TwoWayBinding = {
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }
+  
+  Vue.createApp(TwoWayBinding).mount('#two-way-binding')
