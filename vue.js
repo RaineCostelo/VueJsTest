ee16aad9c4bedac5e5bb62e69675e76dcbb458d9
Lorraine <lorrainejanecostelo@gmail.com>
2021-04-01 13:56:16 +0800

    commit

--- vue.js
+++ vue.js
@@ -60,3 +60,17 @@ const Counter = {
   }
   
   Vue.createApp(ConditionalRendering).mount('#conditional-rendering')
+
+  const ListRendering = {
+    data() {
+      return {
+        todos: [
+          { text: 'Learn JavaScript' },
+          { text: 'Learn Vue' },
+          { text: 'Build something awesome' }
+        ]
+      }
+    }
+  }
+  
+  Vue.createApp(ListRendering).mount('#list-rendering')
